eabbad8239263efd8d68c2c231571d74c374310e
Logan Thompson <logant314159@gmail.com>
2023-04-19 17:00:36 -0500

    Added basic ls functionality.

--- src/bin/ls.rs
+++ src/bin/ls.rs
@@ -1,8 +1,28 @@
+use std::fs;
+
+fn raw_list() -> Vec<String> {
+    let mut list = Vec::new();
+
+    match fs::read_dir(".") {
+        Err(e) => list.push(format!("{e}")),
+        Ok(entries) => {
+            for entry in entries {
+                let result = entry.unwrap().file_name().into_string().unwrap();
+                list.push(result);
+            }
+        }
+    }
+
+    list
+}
+
 fn main() {
     // Parse flags, if any.
 
     // If no flags:
-    //     Just print contents of directory. (Non recursive.)
+    for entry in raw_list() {
+        println!("{entry}");
+    }
 
     // If flags:
     //     Act accordingly.
