1bdfb0d2252255c3a0955095eb100669228494f5
Logan Thompson <logant314159@gmail.com>
2023-04-25 14:08:49 -0500

    Remade primary function responsible for getting files.

--- src/bin/ls.rs
+++ src/bin/ls.rs
@@ -1,15 +1,42 @@
-use std::fs;
+use std::fs::{self, Metadata};
 
-/// Returns a Vec<String> of the items in the current working directory.
-fn raw_list() -> Vec<String> {
+/// The three types of entries ls will differentiate between.
+enum EntryType {
+    Dir,
+    File,
+    Error
+}
+
+/// Returns a Vec<(String, EntryType)> of the item names in the current working directory accompanied by their type.
+fn get_dir_entries_with_type(path: Option<&str>) -> Vec<(String, EntryType)> {
+    // If no path is provided, use the current working directory.
+    let path_str = path.unwrap_or(".");
+
     let mut list = Vec::new();
 
-    match fs::read_dir(".") {
-        Err(e) => list.push(format!("{e}")),
-        Ok(entries) => {
-            for entry in entries {
-                let result = entry.unwrap().file_name().into_string().unwrap();
-                list.push(result);
+    // Iterate over the directory entries and return a Vec<String> of the items in the directory accompanied by their type.
+    // This hurts my soul, imma do my best to explain it. I LOVE Result<T>, I LOOOOVE Result<T>!!!!!!
+    match fs::read_dir(path_str) {
+        Err(e) => list.push((format!("{:?}", e), EntryType::Error)), // If it errors out in the beginning, return an error.
+        Ok(entries_iterator) => {
+            for entry in entries_iterator {
+                match entry {
+                    Err(e) => list.push((format!("{:?}", e), EntryType::Error)), // If any entry individually returns an error, append an error to the vector.
+                    Ok(entry) => {
+                        match entry.metadata() {
+                            Err(e) => list.push((format!("{:?}", e), EntryType::Error)), // If any entry's metadata returns an error, append an error to the vector. (God I love Result<T>!)
+                            Ok(metadata) => {
+                                if metadata.is_dir() {
+                                    list.push((entry.file_name().into_string().unwrap(), EntryType::Dir)); // If the entry is a directory, append a directory entry to the vector.
+                                } else if metadata.is_file() {
+                                    list.push((entry.file_name().into_string().unwrap(), EntryType::File)); // If the entry is a file, append a file entry to the vector.
+                                } else {
+                                    list.push((entry.file_name().into_string().unwrap(), EntryType::Error)); // If the entry is neither a directory nor a file, append an error entry to the vector.
+                                }
+                            }
+                        }
+                    }
+                }
             }
         }
     }
@@ -21,9 +48,6 @@ fn main() {
     // Parse flags, if any.
 
     // If no flags:
-    for entry in raw_list() {
-        println!("{entry}");
-    }
 
     // If flags:
     //     Act accordingly.
